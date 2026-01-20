231a4e63160f2f88b0095df0b7d5f0aa37df784b
Anderson Garcia <andersonmolgarcia@gmail.com>
2026-01-20 15:05:54 -0300

    Criei a edição do perfil do usuário. Alterando os dados no banco de dados.

--- delivery-mais/src/pages/perfil/index.jsx
+++ delivery-mais/src/pages/perfil/index.jsx
@@ -1,8 +1,58 @@
 import './style.css';
 import Navbar from '../../components/navbar';
+import { useEffect, useState } from 'react';
+import api from '../../services/api.js';
 
 
 function Perfil(props) {
+
+
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [mensagem, setMensagem] = useState("");
+    const [erro, setErro] = useState(false);
+
+    const id_usuario = localStorage.getItem('sessionId');
+
+    function ExibeMsg(msg) {
+        setMensagem(msg);
+        setTimeout(() => setMensagem(''), 3000);
+    }
+
+    function SalvarDados() {
+        api.patch(`/v1/usuarios/${id_usuario}`, {
+            nome: nome,
+            email: email
+        })
+            .then(response => {
+                ListarDados();
+                ExibeMsg("Usuário alterado com sucesso!");
+                setErro(false);
+            })
+            .catch(err => {
+                ExibeMsg(err.response.data.erro);
+                setErro(true);
+                console.error('Erro ao alterar usuário: ' + err.response.data.erro);
+            })
+
+    };
+
+    function ListarDados() {
+        api.get(`/v1/usuarios/${id_usuario}`)
+            .then(response => {
+                setNome(response.data[0].nome)
+                setEmail(response.data[0].email)
+                //console.log(response.data)
+            })
+            .catch(err => console.error(err));
+    }
+
+
+    useEffect(() => {
+        ListarDados();
+    }, []);
+
+
 
     return (
 
@@ -19,17 +69,49 @@ function Perfil(props) {
                     <form>
                         <div className='mb-3'>
                             <label htmlFor="InputNome" className='form-label'>Nome</label>
-                            <input type="text" className='form-control' id="InputNome" aria-describedby='nome' />
+                            <input
+                                type="text"
+                                className='form-control'
+                                id="InputNome"
+                                aria-describedby='nome'
+                                onChange={(e) => setNome(e.target.value)}
+                                value={nome}
+                            />
                         </div>
 
                         <div className='mb-3'>
                             <label htmlFor="InputEmail" className='form-label'>E-mail</label>
-                            <input type="email" className='form-control' id="InputEmail" aria-describedby='nome' />
+                            <input
+                                type="email"
+                                className='form-control'
+                                id="InputEmail"
+                                aria-describedby='nome'
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                            />
                         </div>
 
+                        {
+                            mensagem.length > 0 && !erro ?
+                                <div className="alert alert-success text-center mt-4" role="alert">
+                                    {mensagem}
+                                </div>
+                                : null
+                        }
+
+                         {
+                            mensagem.length > 0 && erro ?
+                                <div className="alert alert-danger text-center mt-4" role="alert">
+                                    {mensagem}
+                                </div>
+                                : null
+                        }
+
                         <div className='d-flex justify-content-end'>
-                            <button type='button' className='btn btn-lg btn-danger mt-3'>Salvar Dados</button>
+                            <button onClick={SalvarDados} type='button' className='btn btn-lg btn-danger mt-3'>Salvar Dados</button>
                         </div>
+
+
                     </form>
                 </div>
             </div>
