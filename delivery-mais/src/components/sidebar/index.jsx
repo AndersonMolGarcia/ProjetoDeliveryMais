457e368c611f4e46fa3076924489e3a2ed24264e
Anderson Garcia <andersonmolgarcia@gmail.com>
2026-01-30 10:08:13 -0300

    Criei em implementei o layout para sacola vazia

--- delivery-mais/src/components/sidebar/index.jsx
+++ delivery-mais/src/components/sidebar/index.jsx
@@ -3,6 +3,7 @@ import './style.css';
 import Produto from '../produto/sacola';
 import { useContext, useEffect, useState } from 'react';
 import { CartContext, CartProvider } from '../../contexts/cart';
+import Sacola from '../../assets/bag.png';
 
 
 
@@ -33,80 +34,90 @@ function Sidebar(props) {
                 setShow(visible)
             }}
         >
-            <div className='container-fluid h-100 pt-4 sidebar'>
-                <h5>Minha Sacola</h5>
+            {
+                cart.length == 0 ?
+                <div className='d-flex h-100 flex-column justify-content-center align-items-center text-center'>
+                    <img src={Sacola} alt="Sacola vazia" />
+                    <small className='mt-2 text-secondary'>Sua sacola está vazia</small>
+                </div> 
+                :
+                <div className='container-fluid h-100 pt-4 sidebar'>
+                    <h5>Minha Sacola</h5>
 
-                <div className='row produtos'>
-                    {
-                        cart.map((prod) => {
-                            return <Produto
-                                key={prod.id_carrinho}
-                                nome={prod.nome}
-                                valor_total={prod.vl_unit * prod.qtd}
-                                qtd={prod.qtd}
-                                valor_unit={prod.vl_unit}
-                                id_carrinho={prod.id_carrinho}
-                                url_foto={prod.url_foto}
-                                onClickRemover={ClickRemover}
-                            />
-                        })
-                    }
-                </div>
-
-                <div className='row align-itens-end footer'>
-                    <div className='col-12 d-flex justify-content-between align-itens-center'>
-                        <span>Subtotal</span>
-                        <span>
-                            {
-                                new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(subTotalCart)
-                            }
-                        </span>
+                    <div className='row produtos'>
+                        {
+                            cart.map((prod) => {
+                                return <Produto
+                                    key={prod.id_carrinho}
+                                    nome={prod.nome}
+                                    valor_total={prod.vl_unit * prod.qtd}
+                                    qtd={prod.qtd}
+                                    valor_unit={prod.vl_unit}
+                                    id_carrinho={prod.id_carrinho}
+                                    url_foto={prod.url_foto}
+                                    onClickRemover={ClickRemover}
+                                />
+                            })
+                        }
                     </div>
 
-                    <div className='col-12 d-flex justify-content-between align-itens-center mt-2'>
-                        <div className='input-group'>
-                            <input
-                                type="text"
-                                className='form-control'
-                                placeholder=''
-                                aria-label="Recipient's username"
-                                aria-describedby='button-addon2'
-                            />
-                            <button className='btn btn-outline-success' type='button' id='button-addon2'>Aplicar</button>
-                        </div>
-
-                        <div className='input-group justify-content-end'>
-                            <span className='d-inline-block text-success'>
-                                -
+                    <div className='row align-itens-end footer'>
+                        <div className='col-12 d-flex justify-content-between align-itens-center'>
+                            <span>Subtotal</span>
+                            <span>
                                 {
-                                    new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(descontoCart)
+                                    new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(subTotalCart)
                                 }
                             </span>
                         </div>
 
-                    </div>
+                        <div className='col-12 d-flex justify-content-between align-itens-center mt-2'>
+                            <div className='input-group'>
+                                <input
+                                    type="text"
+                                    className='form-control'
+                                    placeholder=''
+                                    aria-label="Recipient's username"
+                                    aria-describedby='button-addon2'
+                                />
+                                <button className='btn btn-outline-success' type='button' id='button-addon2'>Aplicar</button>
+                            </div>
 
-                    <div className='col-12 d-flex justify-content-between align-itens-center mt-2'>
-                        <span>Taxa de Entrega</span>
-                        <span>
-                            {
-                                new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(entregaCart)
-                            }
-                        </span>
-                    </div>
+                            <div className='input-group justify-content-end'>
+                                <span className='d-inline-block text-success'>
+                                    -
+                                    {
+                                        new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(descontoCart)
+                                    }
+                                </span>
+                            </div>
 
-                    <div className='col-12 d-flex justify-content-between align-itens-center mt-3'>
-                        <b>Total</b>
-                        <h3>
-                            {
-                                new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(totalCart)
-                            }
-                        </h3>
-                    </div>
+                        </div>
 
-                    <button className='btn btn-danger rounded-0 align-itens-center btn-pedido'>Finalizar Pedido</button>
+                        <div className='col-12 d-flex justify-content-between align-itens-center mt-2'>
+                            <span>Taxa de Entrega</span>
+                            <span>
+                                {
+                                    new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(entregaCart)
+                                }
+                            </span>
+                        </div>
+
+                        <div className='col-12 d-flex justify-content-between align-itens-center mt-3'>
+                            <b>Total</b>
+                            <h3>
+                                {
+                                    new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(totalCart)
+                                }
+                            </h3>
+                        </div>
+
+                        <button className='btn btn-danger rounded-0 align-itens-center btn-pedido'>Finalizar Pedido</button>
+                    </div>
                 </div>
-            </div>
+            }
+
+
         </Dock>
     );
 };
